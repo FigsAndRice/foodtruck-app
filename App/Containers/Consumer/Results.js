5acddec069a511b848b7471272c6632e542491c5
Hyein <hyeinu65@gmail.com>
2016-10-13 14:37:52 -0400

    worked on getting the user's current location by lat and lng

--- App/Containers/Consumer/Results.js
+++ App/Containers/Consumer/Results.js
@@ -12,6 +12,8 @@ class Results extends Component {
         this.displayName = 'Results';
 
         this.state = {
+          initialPosition: 'unknown',
+          lastPosition: 'unknown',
           region: {
             latitude: 39.299236,
             longitude: -76.609383
@@ -19,10 +21,41 @@ class Results extends Component {
         }
       this.onRegionChange = this.onRegionChange.bind(this);
     }
+    componentDidMount() {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          let initialPosition = JSON.stringify(position);
+          this.setState({initialPosition});
+        },
+        (error) => alert(JSON.stringify(error)),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+      );
+      this.watchID = navigator.geolocation.watchPosition((position) => {
+        let lastPosition = position;
+        let { latitude, longitude } = position.coords;
+        let region = { latitude, longitude }
+        this.setState({lastPosition, region});
+      });
+    }
+    componentWillUnmount() {
+      navigator.geolocation.clearWatch(this.watchID);
+    }
     onRegionChange(region) {
       this.setState({ region });
     }
     render() {
+      let { coords } = this.state.lastPosition;
+      let lat;
+      let lng;
+
+      if(!coords){
+        lat = "";
+        lng = "";
+      } else {
+        let { latitude, longitude } = coords;
+        lat = latitude;
+        lng = longitude;
+      }
       return (
       	<View>
         <MapView
@@ -35,6 +68,8 @@ class Results extends Component {
             // initialRegion={this.state.region}
             // onRegionChange={this.onRegionChange}
           />
+          <Text>{lat}</Text>
+          <Text>{lng}</Text>
           <RoundedButton onPress={NavigationActions.results}>Search</RoundedButton>
     		</View>
       )
