b69ba3cd7befb9e3c845441bfa78aa80297ddd17
Hyein <hyeinu65@gmail.com>
2016-10-13 15:34:10 -0400

    working on adding react-native-maps as a dependency

--- App/Containers/Consumer/Results.js
+++ App/Containers/Consumer/Results.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, StatusBar, Text, TextInput, StyleSheet, MapView } from 'react-native';
-import RoundedButton from '../Components/RoundedButton';
+import MapView from 'react-native-maps';
+import { View, Text } from 'react-native';
 import { Actions as NavigationActions }  from 'react-native-router-flux';
+
+import RoundedButton from '../Components/RoundedButton';
 
 import styles from '../Styles/RootContainerStyle';
 
@@ -13,10 +15,11 @@ class Results extends Component {
 
         this.state = {
           initialPosition: 'unknown',
-          lastPosition: 'unknown',
           region: {
-            latitude: 39.299236,
-            longitude: -76.609383
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
           }
         }
       this.onRegionChange = this.onRegionChange.bind(this);
@@ -31,10 +34,11 @@ class Results extends Component {
         {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
       );
       this.watchID = navigator.geolocation.watchPosition((position) => {
-        let lastPosition = position;
         let { latitude, longitude } = position.coords;
-        let region = { latitude, longitude }
-        this.setState({lastPosition, region});
+        let latitudeDelta = 0.0922;
+        let longitudeDelta = 0.0421;
+        let region = { latitude, longitude, latitudeDelta, longitudeDelta }
+        this.setState({ region });
       });
     }
     componentWillUnmount() {
@@ -44,34 +48,20 @@ class Results extends Component {
       this.setState({ region });
     }
     render() {
-      let { coords } = this.state.lastPosition;
-      let lat;
-      let lng;
 
-      if(!coords){
-        lat = "";
-        lng = "";
-      } else {
-        let { latitude, longitude } = coords;
-        lat = latitude;
-        lng = longitude;
-      }
       return (
-      	<View>
+
         <MapView
-            style={{ height: 250, marginTop: 80, margin: 20 }}
-            showsUserLocation={true}
+            // style={{ height: 250, marginTop: 80, margin: 20 }}
+            // showsUserLocation={true}
             // followUserLocation={true}
             region={this.state.region}
-            maxDelta={1}
-            minDelta={0.01}
+            // maxDelta={1}
+            // minDelta={0.01}
             // initialRegion={this.state.region}
             // onRegionChange={this.onRegionChange}
           />
-          <Text>{lat}</Text>
-          <Text>{lng}</Text>
-          <RoundedButton onPress={NavigationActions.results}>Search</RoundedButton>
-    		</View>
+
       )
     }
 }
--- android/app/src/main/java/com/foodtruckfinder/MainApplication.java
+++ android/app/src/main/java/com/foodtruckfinder/MainApplication.java
@@ -4,6 +4,11 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,7 +28,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage(),
+            new MapsPackage(),
+            new ReactNativeI18n(),
+            new RNDeviceInfo(),
+            new ReactNativeConfigPackage()
       );
     }
   };
