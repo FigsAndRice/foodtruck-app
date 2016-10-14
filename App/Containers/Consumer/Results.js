a120626cca833b345fefa7452d59603e96bb4893
Hyein <hyeinu65@gmail.com>
2016-10-14 14:44:45 -0400

    enabled maps for ios

--- App/Containers/Consumer/Results.js
+++ App/Containers/Consumer/Results.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
-import { View, StatusBar, Text, TextInput, StyleSheet, MapView } from 'react-native';
+import { Platform,  View, StatusBar, Text, TextInput, StyleSheet, MapView } from 'react-native';
 import RoundedButton from '../Components/RoundedButton';
 import { Actions as NavigationActions }  from 'react-native-router-flux';
 
 import styles from '../Styles/RootContainerStyle';
 
+let markers = [
+  {
+    latitude: 39.299236,
+    longitude: -76.609383,
+    title: 'Center',
+    subtitle: '1234 Foo Drive'
+  },
+  {
+    latitude: 39.249236,
+    longitude: -76.609383,
+    title: 'Foo Place',
+    subtitle: '1234 Foo Drive'
+  },
+  {
+    latitude: 39.349236,
+    longitude: -76.609383,
+    title: 'Foo Place',
+    subtitle: '1234 Foo Drive'
+  },
+  {
+    latitude: 39.299236,
+    longitude: -76.659383,
+    title: 'Foo Place',
+    subtitle: '1234 Foo Drive'
+  },
+  {
+    latitude: 39.299236,
+    longitude: -76.559383,
+    title: 'Foo Place',
+    subtitle: '1234 Foo Drive'
+  },
+  {
+    latitude: 39.298336,
+    longitude: -76.568383,
+    title: 'Foo Place',
+    subtitle: '1234 Foo Drive'
+  }
+]
 
 class Results extends Component {
     constructor(props) {
@@ -23,8 +61,15 @@ class Results extends Component {
       this.setState({ region });
     }
     render() {
+      let Operation;
+      if(Platform.OS === 'ios'){
+        Operation = 'apple';
+      } else {
+        Operation = 'android';
+      }
       return (
       	<View>
+
         <MapView
             style={{ height: 250, marginTop: 80, margin: 20 }}
             showsUserLocation={true}
@@ -34,7 +79,10 @@ class Results extends Component {
             minDelta={0.01}
             // initialRegion={this.state.region}
             // onRegionChange={this.onRegionChange}
+            annotations={markers}
+            zoomEnabled={true}
           />
+          <Text>{Operation}</Text>
           <RoundedButton onPress={NavigationActions.results}>Search</RoundedButton>
     		</View>
       )
