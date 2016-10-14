74bb353c553f4d12afaa89f83ce130bd87d6535c
duhdanube <Danny.B.Lim@gmail.com>
2016-10-14 17:12:59 -0400

    Add logic for geolocation for food truck Profile view

--- App/Containers/Restaurants/Profile.js
+++ App/Containers/Restaurants/Profile.js
@@ -31,11 +31,38 @@ class Profile extends Component {
     // GET TRUE STATE OF OPEN / HOURS FROM BACKEND
     this.state = {
       open: false,
-      hours: 0
+      hours: 0,
+      initialPosition: 'unknown',
+      lastPosition: 'unknown',
+      region: {
+        latitude: 39.299236,
+        longitude: -76.609383
+      }
     }
 
     this.pick = this.pick.bind(this);
     this.open = this.open.bind(this);
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        let initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      let lastPosition = position;
+      let { latitude, longitude } = position.coords;
+      let region = { latitude, longitude }
+      this.setState({lastPosition, region});
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
   }
 
   pick(hours) {
@@ -56,6 +83,13 @@ class Profile extends Component {
 
   // ON RENDER, GET RESTAURANT NAME AND WRITE: "WELCOME, NAME"
   render() {
+    let hours = [1,2,3,4,5,6,7,8,9,10,11,12];
+    let hourItems = hours.map(hour => {
+      return (
+        <Picker.Item key={hour} label={hour.toString()} value={hour.toString()} />
+      )
+    })
+
     let openButton;
     if (this.state.open) {
       openButton = (
@@ -69,18 +103,7 @@ class Profile extends Component {
             selectedValue= {this.state.hours.toString()}
             onValueChange= {this.pick}>
             <Picker.Item label="Hours" value="0" />
-            <Picker.Item label="1" value="1" />
-            <Picker.Item label="2" value="2" />
-            <Picker.Item label="3" value="3" />
-            <Picker.Item label="4" value="4" />
-            <Picker.Item label="5" value="5" />
-            <Picker.Item label="6" value="6" />
-            <Picker.Item label="7" value="7" />
-            <Picker.Item label="8" value="8" />
-            <Picker.Item label="9" value="9" />
-            <Picker.Item label="10" value="10" />
-            <Picker.Item label="11" value="11" />
-            <Picker.Item label="12" value="12" />
+            {hourItems}
           </Picker>
           <GreenButton text="Open" onPress={this.open} />
         </View>
