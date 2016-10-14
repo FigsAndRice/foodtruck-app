93bc4166a4989cd27e63a1496c7f2f2eb17e135e
Hyein <hyeinu65@gmail.com>
2016-10-14 13:27:30 -0400

    Revert to c8a7b88bb19ef4f5e51078b49bce2ef00edcc69a

--- App/Containers/Consumer/Results.js
+++ App/Containers/Consumer/Results.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import MapView from 'react-native-maps';
-import { View, Text } from 'react-native';
-import { Actions as NavigationActions }  from 'react-native-router-flux';
-
+import { View, StatusBar, Text, TextInput, StyleSheet, MapView } from 'react-native';
 import RoundedButton from '../Components/RoundedButton';
+import { Actions as NavigationActions }  from 'react-native-router-flux';
 
 import styles from '../Styles/RootContainerStyle';
 
@@ -14,54 +12,31 @@ class Results extends Component {
         this.displayName = 'Results';
 
         this.state = {
-          initialPosition: 'unknown',
           region: {
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitude: 39.299236,
+            longitude: -76.609383
           }
         }
       this.onRegionChange = this.onRegionChange.bind(this);
-    }
-    componentDidMount() {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          let initialPosition = JSON.stringify(position);
-          this.setState({initialPosition});
-        },
-        (error) => alert(JSON.stringify(error)),
-        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-      );
-      this.watchID = navigator.geolocation.watchPosition((position) => {
-        let { latitude, longitude } = position.coords;
-        let latitudeDelta = 0.0922;
-        let longitudeDelta = 0.0421;
-        let region = { latitude, longitude, latitudeDelta, longitudeDelta }
-        this.setState({ region });
-      });
-    }
-    componentWillUnmount() {
-      navigator.geolocation.clearWatch(this.watchID);
     }
     onRegionChange(region) {
       this.setState({ region });
     }
     render() {
-
       return (
-
+      	<View>
         <MapView
-            // style={{ height: 250, marginTop: 80, margin: 20 }}
-            // showsUserLocation={true}
+            style={{ height: 250, marginTop: 80, margin: 20 }}
+            showsUserLocation={true}
             // followUserLocation={true}
             region={this.state.region}
-            // maxDelta={1}
-            // minDelta={0.01}
+            maxDelta={1}
+            minDelta={0.01}
             // initialRegion={this.state.region}
             // onRegionChange={this.onRegionChange}
           />
-
+          <RoundedButton onPress={NavigationActions.results}>Search</RoundedButton>
+    		</View>
       )
     }
 }
