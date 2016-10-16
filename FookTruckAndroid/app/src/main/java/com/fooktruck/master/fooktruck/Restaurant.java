47d0299160935e1e4e20ff67d3702d6b72e34d0f
Juan Carlos <juancafe2@gmail.com>
2016-10-16 02:05:15 -0400

    Android

---
+++ FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/Restaurant.java
@@ -0,0 +1,12 @@
+package com.fooktruck.master.fooktruck;
+
+import java.util.ArrayList<String>;
+
+/**
+ * Created by MASTER on 10/15/16.
+ */
+public class Restaurant {
+    private String name;
+    private String email;
+
+}
