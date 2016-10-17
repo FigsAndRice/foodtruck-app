b3cbb72c14bb1cb3f078b054123f97496d3c1790
Juan Carlos <juancafe2@gmail.com>
2016-10-17 00:15:27 -0400

    😔

--- FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/LoginActivity.java
+++ FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/LoginActivity.java
@@ -3,15 +3,79 @@ package com.fooktruck.master.fooktruck;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.android.volley.RequestQueue;
 
 public class LoginActivity extends AppCompatActivity {
+    protected static final String Cookie = "COOKIE_SAVE";
+    private int status = -1;
+    private RequestQueue queue;
+    private String cookie = "";
+    protected EditText username;
+    protected EditText password;
+    protected Button login;
+    protected ProgressBar loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Intent i = new Intent(getApplicationContext(), FoodTruckMap.class);
+        initLayout();
+//        Intent i = new Intent(getApplicationContext(), FoodTruckMap.class);
+//
+//        startActivity(i);
+        addListeners();
 
-        startActivity(i);
+    }
+
+    public void initLayout() {
+        username = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.loginbtn);
+        loading = (ProgressBar) findViewById(R.id.loading_profile)
+    }
+    public void addListeners() {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+        username.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    login();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        password.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    login();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void login() {
+        
     }
 }
