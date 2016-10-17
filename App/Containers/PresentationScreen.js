ed3cada1b8e91355c815a31b7aa5b210bf7fe88b
Hyein <hyeinu65@gmail.com>
2016-10-17 16:44:33 -0400

    implemented a current location setting

--- App/Containers/PresentationScreen.js
+++ App/Containers/PresentationScreen.js
@@ -20,38 +20,65 @@ const inputStyle = {
 }
 
 class RootContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.displayName = 'RootContainer';
+  constructor(props) {
+    super(props);
+      this.displayName = 'RootContainer';
 
-        this.state = {
-          search: '',
-          location: ''
-        }
-    }
-    render() {
-      return (
-      	<View style={styles.container} className="container">
-          <Text style={styles.text}>Fook</Text>
-          <TextInput
+      this.state = {
+        search: '',
+        location: '',
+        initialPosition: null,
+        lastPosition: null
+      }
+    this._enterCurrentLocation = this._enterCurrentLocation.bind(this);
+  }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      // var lastPosition = JSON.stringify(position);
+      this.setState({lastPosition: position});
+    });
+  }
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+  _enterCurrentLocation(){
+    let lat = this.state.lastPosition.coords.latitude;
+    let lng = this.state.lastPosition.coords.longitude;
+    this.props.setLocation(lat, lng);
+    NavigationActions.results()
+  }
+  render() {
+    return (
+      <View style={styles.container} className="container">
+        <Text style={styles.text}>Fook</Text>
+        {/* <TextInput
             style={styles.textBox}
             onChangeText={(text) => this.setState({search: text})}
             value={this.state.search}
             autoCorrect={false}
             placeholder="Search"
-          />
-          <View style={inputStyle}>
-          <GooglePlacesAutocomplete
-            placeholder='Enter Location'
-            minLength={2}
-            autoFocus={false}
-            fetchDetails={true}
-            query={{
-              key: 'AIzaSyD82s2ReoEKzyJHzmrJPjf3ogPGgF5s1yE',
-              language: 'en', // language of the results
-              types: '(cities)', // default: 'geocode'
-            }}
-            onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+          /> */}
+        <View style={inputStyle}>
+        <RoundedButton onPress={this._enterCurrentLocation}>Current Location</RoundedButton>
+        <GooglePlacesAutocomplete
+          placeholder='Enter Location'
+          minLength={2}
+          autoFocus={false}
+          fetchDetails={true}
+          query={{
+            key: 'AIzaSyD82s2ReoEKzyJHzmrJPjf3ogPGgF5s1yE',
+            language: 'en', // language of the results
+            types: '(cities)', // default: 'geocode'
+          }}
+          onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
             this.setState({location: details.geometry.location})
             this.props.setLocation(details.geometry.location.lat, details.geometry.location.lng)
           }}
@@ -72,18 +99,17 @@ class RootContainer extends Component {
               color: '#1faadb'
             },
           }}
-          currentLocation={true}
+          // currentLocation={true}
           />
-
-          </View>
-          <RoundedButton onPress={NavigationActions.results}>Search</RoundedButton>
-          <View style={styles.inline}>
-            <YellowButton text="register" onPress={NavigationActions.register} />
-            <YellowButton text="login" onPress={NavigationActions.login} />
-          </View>
-    		</View>
-      )
-    }
+        </View>
+        <RoundedButton onPress={NavigationActions.results}>Search</RoundedButton>
+        <View style={styles.inline}>
+          <YellowButton text="register" onPress={NavigationActions.register} />
+          <YellowButton text="login" onPress={NavigationActions.login} />
+        </View>
+    	</View>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
