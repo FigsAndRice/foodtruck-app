481fae4d4c876a04e140f13b0064c5838a7165c2
Juan Carlos <juancafe2@gmail.com>
2016-10-21 01:28:22 -0400

    I am your war arming the strong aiding the weak know me by name shepherd of fire

--- server/tools/s3_uploader.py
+++ server/tools/s3_uploader.py
@@ -7,10 +7,8 @@ def s3_upload(source_file):
 	source_filename = secure_filename(source_file.filename)
 
 	connect_s3 =  boto.connect_s3(current_app.config['AWS_ACCESS_KEY_ID'], current_app.config['AWS_SECRET_KEY'])
-	#bucket = connect_s3.get_bucket(current_app.config["BUCKET_NAME "])
+	print current_app.config['BUCKET_NAME']
+	bucket = connect_s3.get_bucket(current_app.config['BUCKET_NAME'])
 
-	for bucket in connect_s3.get_all_buckets():
-		print "{name}\t{created}".format(
-                name = bucket.name,
-                created = bucket.creation_date,
-        )
+	key = bucket.new_key(source_filename)
+	key.set_contents_from_string(source_filename)
