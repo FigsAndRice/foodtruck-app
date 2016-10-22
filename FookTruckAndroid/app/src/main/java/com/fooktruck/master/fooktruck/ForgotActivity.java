a424d96ec7e9257a436b6eb7f60df61a144b84d1
Juan Carlos <juancafe2@gmail.com>
2016-10-22 19:44:37 -0400

    Forgot password creasted

---
+++ FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/ForgotActivity.java
@@ -0,0 +1,13 @@
+package com.fooktruck.master.fooktruck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot);
+    }
+}
