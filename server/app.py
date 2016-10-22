7581809d6e50797a40d97115413305a54f1df91c
Juan Carlos <juancafe2@gmail.com>
2016-10-22 15:12:33 -0400

    😔

--- server/app.py
+++ server/app.py
@@ -2,11 +2,10 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask_mail import Mail
 from dotenv import load_dotenv, find_dotenv
-from flask_s3 import FlaskS3
+
 
 db = MongoEngine()
 mail = Mail()
-s3 = FlaskS3()
 load_dotenv(find_dotenv())
 
 def create_app(config_settings):
@@ -15,7 +14,7 @@ def create_app(config_settings):
 	
 	db.init_app(app)
 	mail.init_app(app)
-	s3.init_app(app)
+	
 	# from restaurants.routes import restaurants_app
 	from restaurants.routes import restaurants_app
 	app.register_blueprint(restaurants_app, url_prefix="/api/restaurants")
