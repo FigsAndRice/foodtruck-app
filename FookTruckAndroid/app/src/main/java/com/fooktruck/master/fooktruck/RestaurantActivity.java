8104c4e25febe4ddcf8200f329632a6b5002dd50
Juan Carlos <juancafe2@gmail.com>
2016-10-23 12:37:40 -0400

    Fixing issues

---
+++ FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/RestaurantActivity.java
@@ -0,0 +1,13 @@
+package com.fooktruck.master.fooktruck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+    }
+}
