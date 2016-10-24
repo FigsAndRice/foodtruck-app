a83dd6ba758679fa88206a056048ed24c4ac08ed
Juan Carlos <juancafe2@gmail.com>
2016-10-23 22:28:04 -0400

    Able to get JSON profile info

--- FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/RestaurantActivity.java
+++ FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/RestaurantActivity.java
@@ -1,11 +1,33 @@
 package com.fooktruck.master.fooktruck;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RestaurantActivity extends AppCompatActivity {
     private String cookie = "";
@@ -23,6 +45,37 @@ public class RestaurantActivity extends AppCompatActivity {
     }
 
     public void getProfile() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, URL + "profile" , null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
 
+                        try {
+                            JSONObject obj =  (JSONObject) response.get("results");
+                            Log.d("image link ", (String) obj.get("profile_picture"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Log.d("user's cookie", cookie);
+                Map<String,String> headers = new HashMap<String, String>();
+                headers.put("Cookie", cookie);
+                return headers;
+            }
+
+
+        };
+
+        queue.add(req);
     }
 }
