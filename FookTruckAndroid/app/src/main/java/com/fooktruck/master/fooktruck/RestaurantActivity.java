2f3e8fce56270a9c77b1d96e382a2bbaa9c252e2
Juan Carlos <juancafe2@gmail.com>
2016-10-23 22:12:39 -0400

    Able to save session cookie

--- FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/RestaurantActivity.java
+++ FookTruckAndroid/app/src/main/java/com/fooktruck/master/fooktruck/RestaurantActivity.java
@@ -1,13 +1,28 @@
 package com.fooktruck.master.fooktruck;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class RestaurantActivity extends AppCompatActivity {
-
+    private String cookie = "";
+    final String URL = "http://192.168.1.12:5000/api/restaurants/";
+    protected static final String Cookie = "COOKIE_SAVE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+        SharedPreferences prefs = this.getSharedPreferences(Cookie, MODE_PRIVATE);
+
+        cookie = prefs.getString("Cookie", null);
+        Log.d("cookie ", "I am here " + cookie);
+        getProfile();
+    }
+
+    public void getProfile() {
+
     }
 }
